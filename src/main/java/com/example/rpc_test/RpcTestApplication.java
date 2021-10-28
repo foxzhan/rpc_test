c31fccc851834033094afc0a7156c243854878ab
zhanxyu <414074705@qq.com>
2021-10-28 08:22:07 +0800

    first commit

---
+++ src/main/java/com/example/rpc_test/RpcTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpc_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcTestApplication.class, args);
+    }
+
+}
